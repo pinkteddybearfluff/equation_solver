278223af9c5217d3cddc6414f86ec31c7421ebfa
baphomet_rezy <167787153+pinkteddybearfluff@users.noreply.github.com>
2026-02-19 11:02:18 +0530

    new gaussian elimination for singular matrix

--- matrix_utilities.cpp
+++ matrix_utilities.cpp
@@ -47,6 +47,40 @@ void apply_gaussian(int n_unk, int n_eq, vector<vector<double>>& matrix)
     }
 }
 
+
+void apply_gauss_f_singular(int n_unk, int n_eq, vector<vector<double>>& matrix)
+{
+    for (int i = 0; i < n_eq; ++i)
+    {
+        //check if pivot is non-zero
+        if (matrix[i][i] == 0)
+        {
+            for (int j = i + 1; j < n_eq; ++j)
+            {
+                if (matrix[j][i] != 0)
+                {
+                    //swap rows
+                    for (int k = i; k < n_unk + 1; k++)
+                    {
+                        const double tmp = matrix[j][k];
+                        matrix[j][k] = matrix[i][k];
+                        matrix[i][k] = tmp;
+                    }
+                }
+            }
+        }
+
+        for (int j = i + 1; j < n_eq; ++j)
+        {
+            const double multiplier = matrix[j][i] / matrix[i][i];
+            for (int k = i; k < n_unk + 1; k++)
+            {
+                matrix[j][k] = matrix[j][k] - matrix[i][k] * multiplier;
+            }
+        }
+    }
+}
+
 void apply_jordan(int n_uk, int n_eq, vector<vector<double>>& matrix)
 {
     for (int i = n_eq - 1; i >= 0; --i)
